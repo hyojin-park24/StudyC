d879ca08bcf45cff6ca92fe27f1e8ad021b3d143
hyojin-park24 <gygy332@naver.com>
2021-07-05 19:30:43 +0900

    0705 피라미드 완료 / 0730에 재도전

--- Review Study C/Review Study C2/피라미드.c
+++ Review Study C/Review Study C2/피라미드.c
@@ -4,19 +4,19 @@
 
 int main()
 {
-	int i, j;
-	for  (i = 1; i < 3 ; i++)
+	int i, j,l;
+	for  (i = 1; i < 10 ; i++)
 	{
-		for (j = 1; j < 2; j++)
+		for (j = 0; j < 10-i; j++)
 		{
-			printf("   ");
+			printf(" ");
 		}
-		for ( j = 1; j < 3; j++)
+
+		for ( l = 0; l < i * 2 -1; l++)
 		{
-			printf("*\n");
+			printf("*");
 		}
+		printf("\n");
 	}
-
-
 	return 0;
 }
